342add0b4724391c562f664bbc51f06c951f2ac5
rickyz1st <rickysaputra.jr23@gmail.com>
2022-04-08 11:00:43 +0700

    first commit

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+    Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+                                                                $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
